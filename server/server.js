b7f2efd1a7f3e046d192d65456b715988e443e32
Marc Bouchard <mab.bouchard@gmail.com>
2018-03-25 02:02:20 -0400

    Add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash')
 const express = require('express')
 const bodyParser = require('body-parser')
 
@@ -67,6 +68,32 @@ app.route('/todos/:id')
 				res.send({ todo })
 			})
 			.catch(() => {
+				res.status(400).send()
+			})
+	})
+	.patch(function patchTodosIdCB(req, res) {
+		const { id } = req.params
+
+		const body = _.pick(req.body, ['text', 'completed'])
+
+		if (!idIsValid(id))
+			return res.status(400).send()
+
+		if (_.isBoolean(body.completed) && body.completed) {
+			body.completedAt = new Date().getTime()
+		} else {
+			body.completed = false
+			body.completedAt = null
+		}
+
+		Todo.findByIdAndUpdate(id, { $set: body }, { new: true })	
+			.then((todo) => {
+				if (!todo)
+					return res.status(404).send()
+
+				res.send({ todo })
+			})
+			.catch((error) => {
 				res.status(400).send()
 			})
 	})
